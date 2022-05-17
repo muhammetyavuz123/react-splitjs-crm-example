30204aa28c6e8172ee0d69cb403271485513ace7
muhammetyavuz123 <muhametyavuz@hotmail.com>
2022-05-17 13:32:14 +0300

    delete commit

--- src/Components/GlobalFilter.js
+++ src/Components/GlobalFilter.js
@@ -36,21 +36,3 @@ export const GlobalFilter = ({ filter, setFilter }) => {
     </>
   );
 };
-// export const GlobalFilter = ({ filter, setFilter }) => {
-//   const [value, setValue] = useState(filter);
-//   const onChange = useAsyncDebounce((value) => {
-//     setFilter(value || undefined);
-//   }, 1000);
-//   return (
-//     <span>
-//       Search:{" "}
-//       <input
-//         value={value || ""}
-//         onChange={(e) => {
-//           setValue(e.target.value);
-//           onChange(e.target.value);
-//         }}
-//       />
-//     </span>
-//   );
-// };
